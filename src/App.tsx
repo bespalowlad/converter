2b011b453bccc8541edb76a6b1da6c34e68967e3
Vladyslav <bespalov.v@brights.cloud>
2020-05-12 20:42:08 +0300

    Base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,144 @@
 import React from 'react';
-import './App.css';
+
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(10),
+    },
+
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+
+    inputBox: {
+      marginTop: 20,
+      marginBottom: 20
+    },
+
+    currencyValue: {
+      width: 'calc(70% - 15px)',
+      marginRight: 15
+    },
+
+    currencyType: {
+      width: '30%'
+    }
+  }),
+);
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      App
-    </div>
+    <Container className={classes.root} maxWidth="lg">
+      <div className="App">
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell></TableCell>
+                    <TableCell align="left">FullName</TableCell>
+                    <TableCell align="left">Name</TableCell>
+                    <TableCell align="left">Price</TableCell>
+                    <TableCell align="left">Volume24hour</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="left">{row.calories}</TableCell>
+                      <TableCell align="left">{row.fat}</TableCell>
+                      <TableCell align="left">{row.carbs}</TableCell>
+                      <TableCell align="left">{row.protein}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              <div className={classes.inputBox}>
+                <FormControl className={classes.currencyValue}>
+                  <TextField id="standard-basic" label="Сумма" />
+                </FormControl>
+
+                <FormControl className={classes.currencyType}>
+                  <InputLabel id="demo-simple-select-label">Валюта</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={10}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              <div className={classes.inputBox}>
+                <FormControl className={classes.currencyValue}>
+                  <TextField id="standard-basic" label="Сумма" />
+                </FormControl>
+
+                <FormControl className={classes.currencyType}>
+                  <InputLabel id="demo-simple-select-label">Валюта</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={10}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+
+              <Typography variant="h5" component="h5">
+                h4. Heading
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
   );
 }
 
